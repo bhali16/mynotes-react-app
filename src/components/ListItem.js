6b1ddeab01642a10f7da3afe477275e1cf549ce0
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2023-09-09 02:53:09 +0500

    Update Content and Design

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ListItem = ({note}) => {
   return (
-    <div><p>{note.id} - {note.body}</p></div>
+    <Link to={`/note/${note.id}`}>
+      <div className='notes-list-item'>
+        <h3>{note.body}</h3>
+      </div>
+    </Link>
   )
 }
 
--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import {Link, useParams} from 'react-router-dom'
+import notes from '../assets/data'
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 
 const NotePage = () => {
+  const {id} = useParams();
+
+  let note = notes.find(note => note.id=== Number(id))
   return (
-    <div>NotePage</div>
+    <div className='note'>
+      <div className='note-header'>
+        <h3>
+          <Link to={'/'}>
+            <ArrowLeft/>
+          </Link>
+        </h3>
+      </div>
+      <textarea value={note?.body}></textarea>
+    </div>
   )
 }
 
