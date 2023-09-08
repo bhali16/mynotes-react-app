d7f11d3106654aac58f8a93eaeff336b56709514
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2023-09-09 02:52:52 +0500

    Added Styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import React from 'react'
 import {Outlet} from 'react-router-dom'
 const Header = () => {
   return (
-    <>
-        <h1>My Notes</h1>
+    <div>
+        <h1 className='app-header'>My Notes</h1>
         <Outlet/>
-    </>
+    </div>
   )
 }
 
