57b8d2205a7ae2e46cca7fbb7c2dd44f39ca0953
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2023-09-17 15:05:16 +0500

    Added CreateNote Call

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -14,6 +14,16 @@ const NotePage = () => {
     setNote(data);
   } 
 
+  let createNote = async() => {
+    await fetch(`http://localhost:8000/notes/`,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ ...note, 'updated': new Date() })
+    })
+  }
+
   let updateNote = async() => {
     await fetch(`http://localhost:8000/notes/${id}`,{
       method: 'PUT',
@@ -23,17 +33,6 @@ const NotePage = () => {
       body: JSON.stringify({ ...note, 'updated': new Date() })
     })
   }
-
-  let handleSubmit = () => {
-    console.log('id', id)
-    if(id !==  'new' && !note.body){
-      deleteNote()
-    }else if(id == 'new'){
-      updateNote()
-    }
-    navigate('/');
-  }
-
 
   let deleteNote = async() => {
     await fetch(`http://localhost:8000/notes/${id}`,{
@@ -46,6 +45,19 @@ const NotePage = () => {
 
     navigate('/');
   }
+
+  let handleSubmit = () => {
+    console.log('id', id)
+    if(id !==  'new' && !note.body){
+      deleteNote()
+    }else if(id === 'new'){
+      updateNote()
+    }else if(id === 'new' && note.body !== null){
+      createNote()
+    }
+    navigate('/');
+  }
+
 
 
   useEffect(()=>{
